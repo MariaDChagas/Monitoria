0a7e45e073026bb86bf01421c152bc1a4675b885
MariaDChagas <143622576+MariaDChagas@users.noreply.github.com>
2024-07-23 09:43:16 -0300

    2307

---
+++ servidor2.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded ({ extended: true}))
+app.set('view engine', 'ejs')
+
+app.get('/mon', (request, response) => {
+    resultado = ''
+    response.render('monitoria')
+})
+
+app.post('/rota', (request, response) => {
+    response.render('monitoria', {resultado: ''})
+})
+
+app.post('/soma', (request, response) => {
+    soma = parseFloat(request.body.n1) + parseFloat(request.body.n2)
+    response.render('monitoria', {resultado: soma})
+})
+
+app.post('/sub', (request, response) => {
+    sub = parseFloat(request.body.n1) - parseFloat(request.body.n2)
+    response.render('monitoria', {resultado: sub})
+})
+
+app.post('/mult', (request, response) => {
+    mult = parseFloat(request.body.n1) * parseFloat(request.body.n2)
+    response.render('monitoria', {resultado: mult}) 
+})
+
+app.post('/div', (request, response) => {
+    div = parseFloat(request.body.n1) / parseFloat(request.body.n2)
+    response.render('monitoria', {resultado: div})
+})
+
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log("rodano =D")
+})
+
